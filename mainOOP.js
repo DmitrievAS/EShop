83d05ab6a52eefc9d971bde9c5bd1a3016561d33
DmitrievAS <dmitrich1982@gmail.com>
2021-11-28 08:30:37 +0300

    Переделал на ООП модель c рендингом. Еще не добавил: 1. Корзину 2. Подсчёт суммы товаров в корзине. Вопросы: метод рендер вставил в метод FetchProducts, правильно ли это? Или нужно вставлять в конструктор ProductList (в него не получилось добавить, на момент проведения метода рендер, в массиве объектов goods нет данных с сервера).

--- mainOOP.js
+++ mainOOP.js
@@ -1,3 +1,5 @@
+const API = "https://raw.githubusercontent.com/DmitrievAS/json/main/package.json";
+
 class ProductItems {
     constructor(product) {
         this.id = product.id;
@@ -22,58 +24,124 @@ class ProductList {
     constructor(container = `.wrap-products-main`) {
         this.container = container;
         this.goods = [];
-        this._fetchProducts();
+        this.fetchProducts();
     }
 
-    _fetchProducts() {
-        this.goods = [
-            {
-                id: 1,
-                title: "L-Glutathione",
-                price: 51,
-                description: "Способствует расщеплению окисленных жиров и необходим для углеводного обмена",
-                imgName: "v799-10"
-            },
-            {
-                id: 2,
-                title: "Magnesium Citrate",
-                price: 30,
-                description: "Магний помогает преобразовывать углеводы, белки и жиры в энергию",
-                imgName: "v433-09"
-            },
-            {
-                id: 3,
-                title: "Vitamin C",
-                price: 35,
-                description: "Способствует расщеплению окисленных жиров и необходим для углеводного обмена",
-                imgName: "vtc1_31"
-            },
-            {
-                id: 4,
-                title: "Vitamin G",
-                price: 25,
-                description: "Способствует расщеплению окисленных жиров и необходим для углеводного обмена",
-                imgName: "vtc1_31"
-            },
-            {
-                id: 5,
-                title: "Vitamin A",
-                price: 80,
-                description: "Способствует расщеплению окисленных жиров и необходим для углеводного обмена",
-                imgName: "v799-10"
-            },
-            {
-                id: 6,
-                title: "Vitamin D",
-                price: 67,
-                description: "Способствует расщеплению окисленных жиров и необходим для углеводного обмена",
-                imgName: "v433-09"
-            },
-        ]
+    fetchProducts() {
+        this.goods = fetch(API)
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                this.goods = [...data];
+                this.render();
+            })
+            .catch((onerror) => {
+                alert(`Ошибка при запросе на сервер - ${onerror}`);
+            });
+        // function send() {
+        //     let xhr;
+        //
+        //     if (window.XMLHttpRequest) {
+        //         //Chrome, Mozilla, Opera, Safari
+        //         xhr = new XMLHttpRequest()
+        //     } else if (window.ActiveXObject) {
+        //         xhr = new ActiveXObject("Microsoft.XMLHTTP");
+        //     }
+        //     xhr.open("get", API,true);
+        //     header.forEach((header) => {
+        //         xhr.setRequestHeader(header.key, header.value);
+        //     })
+        //Второй вариант:
+        //  fetch(API)
+        //      .then((response) => {
+        //          console.log(response.json());
+        //          return response.json();
+        //
+        //      })
+        //      .then((request) => {
+        //          console.log(request);
+        //          ProductList.goods = request.map(good => ({
+        //              id: good.id,
+        //              title: good.title,
+        //              price: good.price,
+        //              description: good.description,
+        //              imgName: good.imgName
+        //          }))
+        //      })
+        //      .catch((err) => {
+        //          console.log(err.text);
+        //      });
+
+        // ProductList.goods = fetch(API)
+        //     .then((text) => {
+        //         return text.json();
+        //     })
+        //     .then((data) => {
+        //         ProductList.goods = [...data];
+        //         console.log(ProductList.goods);
+        //     });
+
+        // // _fetchProducts() {
+        //     this.goods = fetch(API)
+        //         .then(text => text.json()) //json метод, который парсит json объект и возвращает в объект промис
+        //         .then(data => { //объект JS
+        //             //this.goods = [...data];
+        //             console.log(data);
+        //             // return data;
+        //             // document.getElementById("app").insertAdjacentHTML("beforeend", `<p>${data.id} - ${data.title}</p>`);
+        //         });
+
+        //     [
+        //     {
+        //         id: 1,
+        //         title: "L-Glutathione",
+        //         price: 51,
+        //         description: "Способствует расщеплению окисленных жиров и необходим для углеводного обмена",
+        //         imgName: "v799-10"
+        //     },
+        //     {
+        //         id: 2,
+        //         title: "Magnesium Citrate",
+        //         price: 30,
+        //         description: "Магний помогает преобразовывать углеводы, белки и жиры в энергию",
+        //         imgName: "v433-09"
+        //     },
+        //     {
+        //         id: 3,
+        //         title: "Vitamin C",
+        //         price: 35,
+        //         description: "Способствует расщеплению окисленных жиров и необходим для углеводного обмена",
+        //         imgName: "vtc1_31"
+        //     },
+        //     {
+        //         id: 4,
+        //         title: "Vitamin G",
+        //         price: 25,
+        //         description: "Способствует расщеплению окисленных жиров и необходим для углеводного обмена",
+        //         imgName: "vtc1_31"
+        //     },
+        //     {
+        //         id: 5,
+        //         title: "Vitamin A",
+        //         price: 80,
+        //         description: "Способствует расщеплению окисленных жиров и необходим для углеводного обмена",
+        //         imgName: "v799-10"
+        //     },
+        //     {
+        //         id: 6,
+        //         title: "Vitamin D",
+        //         price: 67,
+        //         description: "Способствует расщеплению окисленных жиров и необходим для углеводного обмена",
+        //         imgName: "v433-09"
+        //     },
+        // ]
 
     }
+
 
     render() {
+
         if (this.goods.length >= 4) {
             document.querySelector(".wrap-products-main").style.height = 1200 + `px`;
             document.querySelector(".wrap.wrap-products-catalog").style.height = 1500 + `px`;
@@ -89,6 +157,42 @@ class ProductList {
     }
 }
 
-let list = new ProductList();
-list.render();
 
+//Тест на ответ от сервера данных Json,
+// ProductList.goods = fetch(API)
+//     .then((text) => {
+//         return text.json();
+//     })
+// .then((data) => {
+//     ProductList.goods = [...data];
+//     console.log(ProductList.goods);
+// });
+
+
+new ProductList();
+
+// class Basket {
+//     addGoods() {
+//
+//     }
+//
+//     removeGoods() {
+//
+//     }
+//
+//     changeGoods() {
+//
+//     }
+//
+//     render() {
+//
+//     }
+// }
+
+
+// let ProductJSON = ProductItems.ProductList.goods.find(item => item.id == 1);
+// console.log(ProductJSON);
+
+
+// fetch('package.json')
+//     .then(text.json =>text.json())
